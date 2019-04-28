88213d8d20d247f1b75b8d1de9640c7a82eada32
hongghe <364930855@qq.com>
2019-04-28 11:27:41 +0800

    Initial commit

---
+++ src/main/java/com/hongghe/dubbo3example/Dubbo3ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hongghe.dubbo3example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dubbo3ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dubbo3ExampleApplication.class, args);
+    }
+
+}
